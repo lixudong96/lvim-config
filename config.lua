ebb7d2fb32a95796a4bef24bbded306b6cf38487
Sunlee <303001274@qq.com>
2022-09-09 16:14:16 +0800

    feat: add relative numbers

--- config.lua
+++ config.lua
@@ -15,6 +15,8 @@ lvim.colorscheme = "onedarker"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
+-- set relative numbers
+vim.opt.rnu = true
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
